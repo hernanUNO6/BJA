9d5d09f3c9e8fb27ef1298a76c6be7dcec6c4db4
Hernan <hernanyujra@gmail.com>
2013-08-27 00:49:14 -0400

    Establecimientos de Salud

--- Bja.Central.Web/Controllers/EstablecimientosSaludController.cs
+++ Bja.Central.Web/Controllers/EstablecimientosSaludController.cs
@@ -13,7 +13,8 @@ namespace Bja.Central.Web.Controllers
     public class EstablecimientosSaludController : Controller
     {
         private ModeloEstablecimientoSalud modEstableSalud = new ModeloEstablecimientoSalud();
-        private ModeloMunicipio modMunicipio = new ModeloMunicipio();
+        //private ModeloMunicipio modMunicipio = new ModeloMunicipio();
+        private ModeloRedSalud modRedSalud = new ModeloRedSalud();
 
         //
         // GET: /EstablecimientosSalud/
@@ -44,7 +45,10 @@ namespace Bja.Central.Web.Controllers
             ModeloDepartamento modDepto = new ModeloDepartamento();
             ViewBag.IdDepartamento = new SelectList(modDepto.Listar(), "Id", "Descripcion");
 
+            ModeloMunicipio modMunicipio = new ModeloMunicipio();
             ViewBag.IdMunicipio = new SelectList(modMunicipio.Listar(), "Id", "Descripcion");
+
+            ViewBag.IdRedSalud = new SelectList(modRedSalud.Listar(), "Id", "Nombre");
             return View();
         }
 
@@ -54,9 +58,11 @@ namespace Bja.Central.Web.Controllers
         [HttpPost]
         public ActionResult Create(EstablecimientoSalud estableSalud)
         {
+            estableSalud.Id = IdentifierGenerator.NewId();
             estableSalud.IdSesion = 1;
-            estableSalud.FechaUltimaTransaccion = System.DateTime.Now;
-            estableSalud.FechaRegistro = System.DateTime.Now;
+            estableSalud.FechaUltimaTransaccion = DateTime.Now;
+            estableSalud.FechaRegistro = DateTime.Now;
+            estableSalud.EstadoRegistro = TipoEstadoRegistro.Vigente;
 
             if (ModelState.IsValid)
             {
@@ -64,7 +70,7 @@ namespace Bja.Central.Web.Controllers
                 return RedirectToAction("Index");
             }
 
-            ViewBag.IdMunicipio = new SelectList(modMunicipio.Listar(), "Id", "Descripcion", estableSalud.IdMunicipio);
+            ViewBag.IdRedSalud = new SelectList(modRedSalud.Listar(), "Id", "Nombre", estableSalud.IdRedSalud);
             return View(estableSalud);
         }
 
@@ -75,69 +81,73 @@ namespace Bja.Central.Web.Controllers
         {
             EstablecimientoSalud estableSalud = modEstableSalud.Buscar(id);
 
-            estableSalud.Municipio = modMunicipio.Buscar(estableSalud.IdMunicipio);
+            estableSalud.RedSalud = modRedSalud.Buscar(estableSalud.IdRedSalud);
+
+            ModeloMunicipio modMunicipio = new ModeloMunicipio();
+            estableSalud.RedSalud.Municipio = modMunicipio.Buscar(estableSalud.RedSalud.IdMunicipio);
 
             ModeloProvincia modProvincia = new ModeloProvincia();
-            estableSalud.Municipio.Provincia = modProvincia.Buscar(estableSalud.Municipio.IdProvincia);
+            estableSalud.RedSalud.Municipio.Provincia = modProvincia.Buscar(estableSalud.RedSalud.Municipio.IdProvincia);
 
             ModeloDepartamento modDepto = new ModeloDepartamento();
-            estableSalud.Municipio.Provincia.Departamento = modDepto.Buscar(estableSalud.Municipio.Provincia.IdDepartamento);
+            estableSalud.RedSalud.Municipio.Provincia.Departamento = modDepto.Buscar(estableSalud.RedSalud.Municipio.Provincia.IdDepartamento);
 
             if (estableSalud == null)
             {
                 return HttpNotFound();
             }
-            ViewBag.IdDepartamento = new SelectList(modDepto.Listar(), "Id", "Descripcion", estableSalud.Municipio.Provincia.IdDepartamento);
-            ViewBag.IdProvincia = new SelectList(modProvincia.Listar().Where(p => p.IdDepartamento == estableSalud.Municipio.Provincia.IdDepartamento), "Id", "Descripcion", estableSalud.Municipio.IdProvincia);
-            ViewBag.IdMunicipio = new SelectList(modMunicipio.Listar().Where(p => p.IdProvincia == estableSalud.Municipio.IdProvincia), "Id", "Descripcion", estableSalud.IdMunicipio);
+            ViewBag.IdDepartamento = new SelectList(modDepto.Listar(), "Id", "Descripcion", estableSalud.RedSalud.Municipio.Provincia.IdDepartamento);
+            ViewBag.IdProvincia = new SelectList(modProvincia.Listar().Where(p => p.IdDepartamento == estableSalud.RedSalud.Municipio.Provincia.IdDepartamento), "Id", "Descripcion", estableSalud.RedSalud.Municipio.IdProvincia);
+            ViewBag.IdMunicipio = new SelectList(modMunicipio.Listar().Where(p => p.IdProvincia == estableSalud.RedSalud.Municipio.IdProvincia), "Id", "Descripcion", estableSalud.RedSalud.IdMunicipio);
+            ViewBag.cboIdRedSalud = new SelectList(modRedSalud.Listar().Where(p => p.IdMunicipio == estableSalud.RedSalud.IdMunicipio), "Id", "Nombre", estableSalud.IdRedSalud);
             return View(estableSalud);
         }
 
-        //
-        // POST: /EstablecimientosSalud/Edit/5
+        ////
+        //// POST: /EstablecimientosSalud/Edit/5
 
-        [HttpPost]
-        public ActionResult Edit(EstablecimientoSalud estableSalud)
-        {
-            if (ModelState.IsValid)
-            {
-                estableSalud.IdSesion = 1;
-                estableSalud.FechaUltimaTransaccion = System.DateTime.Now;
-                estableSalud.FechaRegistro = System.DateTime.Now;
+        //[HttpPost]
+        //public ActionResult Edit(EstablecimientoSalud estableSalud)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        estableSalud.IdSesion = 1;
+        //        estableSalud.FechaUltimaTransaccion = System.DateTime.Now;
+        //        estableSalud.FechaRegistro = System.DateTime.Now;
 
-                modEstableSalud.Editar(estableSalud);
-                return RedirectToAction("Index");
-            }
-            ViewBag.IdMunicipio = new SelectList(modMunicipio.Listar(), "Id", "Descripcion", estableSalud.IdMunicipio);
-            return View(estableSalud);
-        }
+        //        modEstableSalud.Editar(estableSalud);
+        //        return RedirectToAction("Index");
+        //    }
+        //    ViewBag.IdMunicipio = new SelectList(modMunicipio.Listar(), "Id", "Descripcion", estableSalud.IdMunicipio);
+        //    return View(estableSalud);
+        //}
 
-        //
-        // GET: /EstablecimientosSalud/Delete/5
+        ////
+        //// GET: /EstablecimientosSalud/Delete/5
 
-        public ActionResult Delete(long id = 0)
-        {
-            EstablecimientoSalud estableSalud = modEstableSalud.Buscar(id);
-            if (estableSalud == null)
-            {
-                return HttpNotFound();
-            }
-            return View(estableSalud);
-        }
+        //public ActionResult Delete(long id = 0)
+        //{
+        //    EstablecimientoSalud estableSalud = modEstableSalud.Buscar(id);
+        //    if (estableSalud == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(estableSalud);
+        //}
 
-        //
-        // POST: /EstablecimientosSalud/Delete/5
+        ////
+        //// POST: /EstablecimientosSalud/Delete/5
 
-        [HttpPost, ActionName("Delete")]
-        public ActionResult DeleteConfirmed(long id)
-        {
-            modEstableSalud.Eliminar(id);
-            return RedirectToAction("Index");
-        }
+        //[HttpPost, ActionName("Delete")]
+        //public ActionResult DeleteConfirmed(long id)
+        //{
+        //    modEstableSalud.Eliminar(id);
+        //    return RedirectToAction("Index");
+        //}
 
         protected override void Dispose(bool disposing)
         {
-            
+
         }
 
         [ActionName("ProvinciasPorDepartamento")]
@@ -153,9 +163,20 @@ namespace Bja.Central.Web.Controllers
         [ActionName("MunicipiosPorProvincia")]
         public ActionResult GetMunicipiosPorProvincias(string id)
         {
+            ModeloMunicipio modMunicipio = new ModeloMunicipio();
+
             List<Municipio> Datos = modMunicipio.GetMunicipiosPorProvincias(id);
             var myData = (from d in Datos select new { d.Id, d.Descripcion });
             return Json(myData, JsonRequestBehavior.AllowGet);
         }
+
+        [ActionName("RedesSaludPorMunicipio")]
+        public ActionResult GetRedesSaludPorMunicipio(string id)
+        {
+            List<RedSalud> Datos = modRedSalud.GetRedesSaludPorMunicipio(id);
+            var myData = (from d in Datos select new { d.Id, Descripcion = d.Nombre });
+            return Json(myData, JsonRequestBehavior.AllowGet);
+        }
+
     }
 }
